2601d153b10d83ef462dafe6eacd0b60059186f1
Takagineer <itengineer0415@gmail.com>
2024-05-27 22:12:20 +0900

    firstCommit

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+{
+  function showHeader() {
+    console.log("showHeader");
+  }
+  function showUsers() {
+    console.log("showUsers");
+  }
+  function showFooter() {
+    console.log("showFooter");
+  }
+
+  showHeader();
+  showUsers();
+  showFooter();
+}
