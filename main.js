9530e9382cc6c8b16864104e4928e14213ad36de
Takagineer <itengineer0415@gmail.com>
2024-05-27 22:36:44 +0900

    update/allowfunction

--- main.js
+++ main.js
@@ -3,17 +3,19 @@
   function showHeader() {
     console.log("showHeader");
   }
-  async function showUsers() {
+
+  const showUsers = async () => {
     try {
       const response = await fetch(
-        "https://dotinstall.github.io/setup/fetchapi/invalid-users.json"
+        "https://dotinstall.github.io/setup/fetchapi/users.json"
       );
       const users = await response.json();
       console.log(users);
     } catch (err) {
       console.log(err);
     }
-  }
+  };
+
   function showFooter() {
     console.log("showFooter");
   }
